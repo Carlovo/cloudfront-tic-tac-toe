d86bb72a6f92079040429c1f6d43179ab9ee47d5
Carlo van Overbeek <carlovo@gmail.com>
2023-09-12 21:31:14 +0200

    validate game state in computer move function

--- content/api-code/get-computer-move.js
+++ content/api-code/get-computer-move.js
@@ -1,25 +1,40 @@
 var crypto = require('crypto');
 
-function handler(event) {
-  var gameState = event.request.querystring.gameState.value
-  var playerMove = parseInt(event.request.querystring.playerMove.value)
+// Function to ensure a constant time comparison to prevent timing side channels.
+function _constantTimeEquals(a, b) {
+  if (a.length == b.length) {
+    var xor = 0;
 
-  if (gameState.charAt(playerMove) == '0') {
+    for (var i = 0; i < a.length; i++) {
+      xor |= (a.charCodeAt(i) ^ b.charCodeAt(i))
+    }
+
+    return 0 === xor
+  } else {
+    return false
+  }
+}
+
+function _calculateDigest(data) {
+  return crypto
+    .createHmac('sha256', '${ signing_key }')
+    .update(data)
+    .digest('base64url')
+}
+
+function handler(event) {
+  var querystring = event.request.querystring
+  var gameState = querystring.gameState.value
+  var playerMove = parseInt(querystring.playerMove.value)
+
+  if (_constantTimeEquals(querystring.signature.value, _calculateDigest(gameState))
+    && gameState.charAt(playerMove) == '0') {
     var playerState = gameState.substring(0, playerMove) + '2' + gameState.substring(playerMove + 1)
     var computerMove = playerState.indexOf('0')
-    if (computerMove > -1) {
-      var computerState = playerState.substring(0, computerMove) + '1' + playerState.substring(computerMove + 1)
-    } else {
-      var computerState = playerState
-    }
+    var computerState = playerState.substring(0, computerMove) + '1' + playerState.substring(computerMove + 1)
   } else {
     var computerState = 'impossible'
   }
-
-  var signature = crypto
-    .createHmac('sha256', '${ signing_key }')
-    .update(computerState)
-    .digest('base64url')
 
   return {
     statusCode: 200,
@@ -31,6 +46,6 @@ function handler(event) {
         'value': 'text/plain'
       }
     },
-    body: computerState + "." + signature
+    body: computerState + "." + _calculateDigest(computerState)
   }
 }
